7bfb5ee0cbc913ee859bf60f928ea9b14d299fb0
Sean Clayton <clayton.sean@gmail.com>
2018-01-03 14:15:23 -0500

    Create filter action

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,4 +1,11 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    filterByCity(param) {
+      return param !== ''
+        ? this.get('store').query('rental', { city: param })
+        : this.get('store').findAll('rental');
+    }
+  }
 });
