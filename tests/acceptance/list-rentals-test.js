6530879d09d4804b711eaa0ba29533cbf73c3f30
Sean Clayton <clayton.sean@gmail.com>
2018-01-03 12:03:45 -0500

    Update tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -26,7 +26,12 @@ test('should link to contact information.', assert => {
   });
 });
 
-test('should list available rentals.', assert => {});
+test('should list available rentals.', assert => {
+  visit('/');
+  andThen(() => {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
 
 test('should filter the list of rentals by city', assert => {});
 
