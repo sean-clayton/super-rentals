484bc3a589147e7aa8ee0a2812f1f2d57e2c3792
Sean Clayton <clayton.sean@gmail.com>
2018-01-03 14:59:44 -0500

    Add debouncing
    It was hurting my soul doing all those
    network requests :(

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+import { debounce } from '@ember/runloop';
 
 export default Component.extend({
   classNames: ['list-filter'],
@@ -6,14 +7,20 @@ export default Component.extend({
 
   init() {
     this._super(...arguments);
-    this.get('filter')('').then(results => this.set('results', results));
+    this.get('filter')('').then(({ results }) => this.set('results', results));
+  },
+
+  filterItems() {
+    let filterInputValue = this.get('value');
+    let filterAction = this.get('filter');
+    filterAction(filterInputValue).then(({ query, results }) => {
+      if (query === this.get('value')) this.set('results', results)
+    });
   },
 
   actions: {
     handleFilterEntry() {
-      let filterInputValue = this.get('value');
-      let filterAction = this.get('filter');
-      filterAction(filterInputValue).then(filterResults => this.set('results', filterResults));
+      debounce(this, this.filterItems, 200);
     }
   }
 });
