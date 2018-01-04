3751010b78861222c33ee1fb9f1dcd0b108ccd7e
Sean Clayton <clayton.sean@gmail.com>
2018-01-04 11:38:58 -0500

    More refactoring

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "./config/environment";
 
 const Router = EmberRouter.extend({
   location: config.locationType,
@@ -7,10 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
-  this.route('rentals', function() {
-    this.route('show');
+  this.route("about");
+  this.route("contact");
+  this.route("rentals", function() {
+    this.route("show", { path: "/:rental_id" });
   });
 });
 
