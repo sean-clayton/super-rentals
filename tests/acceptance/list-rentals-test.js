002667d304139a90decb993129da01cef0de63a3
Sean Clayton <clayton.sean@gmail.com>
2018-01-04 11:54:42 -0500

    Finish acceptance tests

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -63,4 +63,24 @@ test("should filter the list of rentals by city", assert => {
   });
 });
 
-test("should show the details for a selected rental", () => {});
+test("should show the details for a selected rental", assert => {
+  visit("/rentals");
+  click('a:contains("Grand Old Mansion")');
+  andThen(() => {
+    assert.equal(
+      currentURL(),
+      "/rentals/grand-old-mansion",
+      "should navigate to show route"
+    );
+    assert.equal(
+      find(".show-listing h2").text(),
+      "Grand Old Mansion",
+      "should list rental title"
+    );
+    assert.equal(
+      find(".description").length,
+      1,
+      "should list a description of the property"
+    );
+  });
+});
