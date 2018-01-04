e8380c9a9d2bac8f36c1ef3f40aace655e57349f
Sean Clayton <clayton.sean@gmail.com>
2018-01-04 10:15:31 -0500

    Hook location-map component to service

--- app/components/location-map.js
+++ app/components/location-map.js
@@ -1,4 +1,13 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
+import { inject as service } from "@ember/service";
 
 export default Component.extend({
+  maps: service(),
+
+  didInsertElement() {
+    this._super(...arguments);
+    const location = this.get("location");
+    const mapElement = this.get("maps").getMapElement(location);
+    this.$(".map-container").append(mapElement);
+  }
 });
