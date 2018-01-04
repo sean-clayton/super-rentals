a701f06ad7c581d36e77993634b044c1e974a6f1
Sean Clayton <clayton.sean@gmail.com>
2018-01-04 11:33:52 -0500

    Some refactoring

--- mirage/config.js
+++ mirage/config.js
@@ -1,42 +1,63 @@
 export default function() {
-  this.namespace = '/api';
+  this.namespace = "/api";
 
-  const rentals = [{
-    type: 'rentals',
-    id: 'grand-old-mansion',
-    attributes: {
-      title: 'Grand Old Mansion',
-      owner: 'Veruca Salt',
-      city: 'San Francisco',
-      "property-type": 'Estate',
-      bedrooms: 15,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg'
+  const rentals = [
+    {
+      type: "rentals",
+      id: "grand-old-mansion",
+      attributes: {
+        title: "Grand Old Mansion",
+        owner: "Veruca Salt",
+        city: "San Francisco",
+        "property-type": "Estate",
+        bedrooms: 15,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+      }
+    },
+    {
+      type: "rentals",
+      id: "urban-living",
+      attributes: {
+        title: "Urban Living",
+        owner: "Mike Teavee",
+        city: "Seattle",
+        "property-type": "Condo",
+        bedrooms: 1,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+      }
+    },
+    {
+      type: "rentals",
+      id: "downtown-charm",
+      attributes: {
+        title: "Downtown Charm",
+        owner: "Violet Beauregarde",
+        city: "Portland",
+        "property-type": "Apartment",
+        bedrooms: 3,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+      }
     }
-  }, {
-    type: 'rentals',
-    id: 'urban-living',
-    attributes: {
-      title: 'Urban Living',
-      owner: 'Mike Teavee',
-      city: 'Seattle',
-      "property-type": 'Condo',
-      bedrooms: 1,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg'
-    }
-  }, {
-    type: 'rentals',
-    id: 'downtown-charm',
-    attributes: {
-      title: 'Downtown Charm',
-      owner: 'Violet Beauregarde',
-      city: 'Portland',
-      "property-type": 'Apartment',
-      bedrooms: 3,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg'
-    }
-  }]
-  
-  this.get('/rentals', (_db, {queryParams: {city}}) => city !== undefined
-    ? { data: rentals.filter(r => r.attributes.city.toLowerCase().indexOf(city.toLowerCase()) !== -1) }
-    : { data: rentals })
+  ];
+
+  this.get(
+    "/rentals",
+    (_db, { queryParams: { city } }) =>
+      city !== undefined
+        ? {
+            data: rentals.filter(
+              r =>
+                r.attributes.city.toLowerCase().indexOf(city.toLowerCase()) !==
+                -1
+            )
+          }
+        : { data: rentals }
+  );
+
+  this.get("/rentals/:id", (_db, { params: { id } }) => ({
+    data: rentals.find(rental => id === rental.id)
+  }));
 }
